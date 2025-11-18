c67cae52eb0520422c2925c13dd88183c393c60b
Jérôme Picault <jerome.picault@rte-france.com>
2025-11-18 13:40:26 +0100

    chore: improved logging for newsletters

--- bertrend_apps/services/routers/newsletters.py
+++ bertrend_apps/services/routers/newsletters.py
@@ -7,13 +7,16 @@ import asyncio
 from fastapi import APIRouter, HTTPException
 from loguru import logger
 
-
+from bertrend import load_toml_config
 from bertrend_apps import SCHEDULER_UTILS
-from bertrend_apps.newsletters.newsletter_generation import process_newsletter
+from bertrend_apps.newsletters.newsletter_generation import (
+    process_newsletter,
+    NEWSLETTER_SECTION,
+)
 
 from bertrend_apps.services.config.settings import get_config
 from bertrend_apps.services.models.newsletters_models import NewsletterRequest
-
+from bertrend_apps.services.utils.logging_utils import get_file_logger
 
 # Load the configuration
 CONFIG = get_config()
@@ -29,6 +32,12 @@ async def newsletter_from_feed(req: NewsletterRequest):
     """
     Creates a newsletter associated to a data feed.
     """
+    config = load_toml_config(req.newsletter_toml_path)
+    model_id = config.get(NEWSLETTER_SECTION).get("id")
+    # Create a unique log file for this call
+    logger_id = get_file_logger(
+        id="generate_newsletters", user_name="", model_id=model_id
+    )
     try:
         await asyncio.to_thread(
             process_newsletter,
@@ -39,6 +48,9 @@ async def newsletter_from_feed(req: NewsletterRequest):
     except Exception as e:
         logger.error(f"Error generating newsletter: {e}")
         raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        # Remove the logger to prevent writing to this file for other calls
+        logger.remove(logger_id)
 
 
 @router.post("/schedule-newsletters", summary="Schedule newsletter automation")
