7404921413456b1798794dbde53ce00c0ae4d484
Jérôme Picault <jerome.picault@rte-france.com>
2025-12-08 18:09:37 +0100

    chore: test script

---
+++ check_opened_files.sh
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2024, RTE (https://www.rte-france.com)
+# See AUTHORS.txt
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of BERTrend.
+#
+
+for pid in /proc/[0-9]*; do
+    p=${pid##*/}                                # extract pid number
+    fdcount=$(ls "$pid/fd" 2>/dev/null | wc -l)
+    cmd=$(tr -d '\0' < "$pid/comm" 2>/dev/null)
+    printf "%-6s %-6s %s\n" "$p" "$fdcount" "$cmd"
+done | sort -k2 -n
+
